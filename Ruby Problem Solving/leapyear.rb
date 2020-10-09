02b01644acbf72dfc77ffc719d189ab0595ca0f1
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-09 13:44:44 -0400

    add fully functional leapyear script/solution

---
+++ Ruby Problem Solving/leapyear.rb
@@ -0,0 +1,28 @@
+# write a Ruby program which accept the a year from the user and determines if that year is a leap year or not. 
+# If they user enters anything other then a valid year, the application should re-prompt the user for to enter a valid year.
+
+# instantiate the input variable
+input = nil
+
+# keep asking for a number until one is provided
+loop do
+  print "Enter a year: "
+  input = gets.chomp
+  
+  # this is how we check that the string entered is an actual integer
+  # by converting the string to an int and then back to a string  
+  # then checking that it has the same value as the original input
+  if (input.to_i.to_s == input && input.to_i > 0)
+    break
+  else
+    puts "#{input} is not a valid year"
+  end
+end
+
+year_num = input.to_i
+
+if ((year_num % 4 == 0 && !(year_num % 100 == 0)) || year_num % 400 == 0)
+  puts "#{input} is a leap year"
+else
+  puts "#{input} is not a leap year"
+end
