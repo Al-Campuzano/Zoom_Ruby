6ad235194349437d89aef62c2a823c9ba442fbb0
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-09 08:15:17 -0400

    add comments, rename variables to make more sense

--- Ruby Problem Solving/rearrange.rb
+++ Ruby Problem Solving/rearrange.rb
@@ -1,5 +1,5 @@
-#write a Ruby program that given an integer number, rearranges its digits to create the largest number possible with those digits. 
-#If a non-integer number is entered, it should reprompt the person again for a number until a number is entered.
+# Write a Ruby program that given an integer number, rearranges its digits to create the largest number possible with those digits. 
+# If a non-integer number is entered, it should reprompt the person again for a number until a number is entered.
 
 # instantiate the input variable
 input = nil
@@ -8,6 +8,8 @@ input = nil
 loop do
   print "Enter a number: "
   input = gets.chomp
+  # if the user entered 0 or -0 then exit the progam
+  # thus avoiding dividing by zero later on when we check if the number entered is positive or negative
   if input.eql?("-0") || input.eql?("0")
     puts input
     return
@@ -18,31 +20,29 @@ loop do
   break if input.to_i.to_s == input
 end
 
-# crappy way, does not work if input is -105
-# if num.to_i >= 0
-#   puts num.split("").sort.join("").reverse
-# else
-#   puts "-#{num.split("").drop(1).sort.join("")}"  
-# end
-
-# new idea
-
 num = input.to_i
+# if positive then multiplier will be 1, if negative it will be -1
 multiplier = num / num.abs
+# array of characters that we can then use to find all combinations
 num_arr = input.split("")
 
+# if number is negative then remove the first item in the array because it will be the minus (-) sign
 if multiplier == -1
   num_arr = num_arr.drop(1)
 end
 
-perms = num_arr.permutation.to_a
-options = []
+# get all the permutations from the chars in the array
+permutations = num_arr.permutation.to_a
+possible_options = []
 
-for n in perms do
-  unless n[0].eql?("0")
-    options.append(n.join("").to_i * multiplier)
+# loop through the permutations and for those that don't start with zero join them into a string,
+# convert them to an integer, multiply them by either 1 or -1 and add it to the options array
+for p in permutations do
+  unless p[0].eql?("0")
+    possible_options.append(p.join("").to_i * multiplier)
   end
 end
 
-puts options.max
+# output the largest number in the options array
+puts possible_options.max
 
