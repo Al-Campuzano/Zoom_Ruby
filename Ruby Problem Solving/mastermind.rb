e528e09aecc2390c15c347a18b28c791a42dd801
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-12 03:24:24 -0400

    Add comments

--- Ruby Problem Solving/mastermind.rb
+++ Ruby Problem Solving/mastermind.rb
@@ -9,63 +9,48 @@
 puts "MASTERMIND"
 
 # generate random 4-digit code with digits from 1 to 6
-limit = 6
+limit = 6 # highest digit that can be used
 r_code = ""
 4.times do
   r_code += (rand(limit) + 1 ).to_s
 end
-# puts r_code
 
-counter = 1
+counter = 1 # keeps track of number of tries/guesses
 loop do
   print "[#{counter}] Enter your guess: "
   u_guess = gets.chomp
 
-  # check that input string is exactly 4 characters, is a number, the first character is not a minus sign, and no digit is larger than 6 or less than 1
+  # check that input string is exactly 4 characters, is a number, the first character is not a minus sign, and no digit is larger than limit or less than 1
   if (u_guess.length == 4 && u_guess.to_i.to_s == u_guess && !u_guess[0].eql?("-") && !u_guess.split("").any?{|n| n.to_i > limit || n.to_i < 1})
+    # if the guess is the same as the random code then print the code and a string congratulating the user
     if (u_guess.eql?(r_code))
       puts u_guess
       guess_str = "guess"
-      counter > 1 ? guess_str += "es." : guess_str += "." 
+      counter > 1 ? guess_str += "es." : guess_str += "." # pluralize the word "guess" if needed
       puts "Nice work! You took #{counter} #{guess_str}"
       break
-    else
-      counter += 1
-      output = ""
-      code_copy = r_code.clone
+    else # the guess was incorrect
+      counter += 1 # increase the number of guesses by one
+      output = "" # used to build the string with hints
+      code_copy = r_code.clone # clone the random code so we can remove characters that have been matched
+      # loop through the guess string's characters and check if any of the guessed characters are in the exact position as in the random code
       u_guess.each_char.with_index { |c,i|
         if r_code[i].eql?(u_guess[i])
+          # remove the character from the copy of the code so it can't be matched again later
           code_copy[code_copy.index(c)] = ""
-          # puts "Copy: #{code_copy}"
+          # add the correct hint character to the output string
           output += "X"
         end
       }
-      # puts "Random code: #{r_code}, code copy: #{code_copy}"
+
+      # loop through guess string's chars to see if any of them are in any part of the remaining cloned code, if so then remove that char from the cloned code and add the correct hint char to the output string
       u_guess.each_char { |c|
         if code_copy.include?(c)
           code_copy[code_copy.index(c)] = ""
-          # puts "Copy: #{code_copy}"
           output += "x"
         end
       }
-      puts output
+      puts output # print the hint text
     end
   end
 end
-
-# testStr = "12"
-# testIn = "22"
-# result = ""
-
-# testIn.each_char.with_index { |c,i| 
-#   newStr = ""
-#   if testStr.include?(c)
-#     newStr = "x"
-#     if testStr[i].eql?(testIn[i])
-#       newStr.upcase!
-#     end
-#   end
-#   result += newStr
-# }
-
-# puts result
