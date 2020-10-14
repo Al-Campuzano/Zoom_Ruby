0f29220b65ef7d856e64aaf07ef39bf47f535145
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-14 09:14:13 -0400

    refactor a bit, changed hash values to be outcome strings

--- rock_paper_scissors/rps_part1.rb
+++ rock_paper_scissors/rps_part1.rb
@@ -1,26 +1,21 @@
 # Write a Ruby program that asks two players to enter either Rock, Paper or Scissors and then outputs the outcome of the game.
 # Note: You should check whether there is valid. If it is invalid, say "No cheaters! Only Rock, Paper or Scissors are allowed.
 
-outcome_hash = {"rock" => "scissors", "scissors" => "paper", "paper" => "rock"}
+outcome_hash = {"rock" => "Rock crushes scissors.", "scissors" => "Scissors cuts paper.", "paper" => "Paper covers rock."}
 invalid_msg = "No cheaters! Only Rock, Paper or Scissors are allowed."
 
 puts "Rock Paper Scissors" # game title
 
 print "Player 1 - Enter your selection: "
-player_one = gets.chomp
+player_one = gets.chomp.downcase
 
-unless outcome_hash.key?(player_one.downcase)
-  puts invalid_msg
+if outcome_hash.key?(player_one.downcase)
+  print "Player 2 - Enter your selection: "
+  player_two = gets.chomp.downcase
+  puts invalid_msg unless outcome_hash.key?(player_two)
+else
+  puts invalid_msg  
 end
 
-print "Player 2 - Enter your selection: "
-player_two = gets.chomp
-
-unless outcome_hash.key?(player_two.downcase)
-  puts invalid_msg
-end
-
-if player_one.downcase == player_two.downcase
-  puts "Tie!"
-end
+puts "Tie!" if player_one == player_two
 
