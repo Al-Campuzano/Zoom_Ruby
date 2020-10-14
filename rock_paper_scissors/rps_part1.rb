3a6a18f983bf2e5726c924fecf79a8ea51f0e42c
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-14 09:24:57 -0400

    check winner using outcome hash. Add comments

--- rock_paper_scissors/rps_part1.rb
+++ rock_paper_scissors/rps_part1.rb
@@ -1,7 +1,9 @@
 # Write a Ruby program that asks two players to enter either Rock, Paper or Scissors and then outputs the outcome of the game.
 # Note: You should check whether there is valid. If it is invalid, say "No cheaters! Only Rock, Paper or Scissors are allowed.
 
+# use this hash to map an object (rock, paper, etc) to it's winning outcome
 outcome_hash = {"rock" => "Rock crushes scissors.", "scissors" => "Scissors cuts paper.", "paper" => "Paper covers rock."}
+# invalid input message
 invalid_msg = "No cheaters! Only Rock, Paper or Scissors are allowed."
 
 puts "Rock Paper Scissors" # game title
@@ -9,6 +11,7 @@ puts "Rock Paper Scissors" # game title
 print "Player 1 - Enter your selection: "
 player_one = gets.chomp.downcase
 
+# if player one's input is valid get player two's input, else print invalid message
 if outcome_hash.key?(player_one.downcase)
   print "Player 2 - Enter your selection: "
   player_two = gets.chomp.downcase
@@ -19,3 +22,9 @@ end
 
 puts "Tie!" if player_one == player_two
 
+# if player two's choice is in the winning message of player one's choice then player one wins, else player two wins
+if outcome_hash[player_one].include?(player_two)
+  puts outcome_hash[player_one]
+else
+  puts outcome_hash[player_two]
+end
