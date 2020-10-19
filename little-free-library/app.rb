291d581968f735fe5b10a4783f7cabebbcd8023a
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-19 11:01:24 -0400

    initialize branch

---
+++ little-free-library/app.rb
@@ -0,0 +1,9 @@
+# Checklist
+
+# All Books have a Title and a Page Count
+# The Library can have a maximum of 18 Books
+# A User can Look inside the Library
+# A User can Take a Book from the Library
+# A User can Place a Book in the Library
+# All input is checked, and validated
+
