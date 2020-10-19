88bf8996d1299cf3d830d2c144edd149ba2f81a8
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-19 11:28:33 -0400

    add Library and Book classes, inits and print methods

--- little-free-library/app.rb
+++ little-free-library/app.rb
@@ -7,3 +7,34 @@
 # A User can Place a Book in the Library
 # All input is checked, and validated
 
+class Library
+  def initialize
+    @books = []
+  end
+
+  def look
+    @books.each { |book| book.print_book }
+  end
+
+  def place new_book
+    @books.push(new_book)
+  end
+end
+
+class Book
+  def initialize(title, pages)
+    @title = title
+    @pages = pages
+  end
+
+  def print_book
+    puts "- #{@title}, with #{@pages} pages."
+  end
+end
+
+lib = Library.new
+b1 = Book.new("A Study in Scarlet", 200)
+lib.place(b1)
+puts b1.class
+
+lib.look
