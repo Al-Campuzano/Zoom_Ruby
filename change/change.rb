1f9664206951169a51fbad0d76411222f0f95a78
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-20 15:33:38 -0400

    full solution with user input

--- change/change.rb
+++ change/change.rb
@@ -1,5 +1,3 @@
-test_value = 343
-
 def calculate_results(cents)
   coins = {"toonie" => 200, "loonie" => 100, "quarter" => 25, "dime" => 10, "nickel" => 05}
   coins_back = {}
@@ -15,7 +13,7 @@ def calculate_results(cents)
   end
 
   if cents > 2
-    coins_back["nickel"] += 1
+    coins_back["nickel"] ? coins_back["nickel"] += 1 : coins_back["nickel"] = 1
     total_coins += 1
   end
 
@@ -23,20 +21,29 @@ def calculate_results(cents)
 end
 
 def build_response(coins_hash)
+  return "You don't need to dispense change." if coins_hash.length == 0
+
   respond_string = ""
 
   coins_hash.each_with_index do |(name, value), index|
-    if index == coins_hash.length - 1 
+    if (index == coins_hash.length - 1 && coins_hash.length > 1)
       respond_string += "and "
     end
     respond_string += "#{value} #{name}#{value > 1 ? "s" : ""}, "
   end
 
-  "#{respond_string[0..-3]}."
+  "You need to dispense #{respond_string[0..-3]}."
 end
 
+user_input = ""
 
-result = calculate_results(test_value)
+loop do
+  print "How much change is owed? "
+  user_input = gets.chomp
+  break if user_input.to_f > 0
+end
 
-puts "You need to dispense #{build_response(result["dispense"])}"
+result = calculate_results((user_input.to_f * 100).round)
+
+puts build_response(result["dispense"])
 puts "Total coins: #{result["total"]}"
