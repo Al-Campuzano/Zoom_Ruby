0433733fac461476d5af57d065caec5b80a8693f
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-20 14:52:35 -0400

    refactored into methods

--- change/change.rb
+++ change/change.rb
@@ -1,35 +1,42 @@
-coins = {"toonie" => 200, "loonie" => 100, "quarter" => 25, "dime" => 10, "nickel" => 05}
 test_value = 343
-coins_back = {}
-total_coins = 0
 
-coins.each do |name, value|
-  coins_to_add = test_value/value
-  unless coins_to_add == 0
-    total_coins += coins_to_add
-    coins_back[name] = coins_to_add
+def calculate_results(cents)
+  coins = {"toonie" => 200, "loonie" => 100, "quarter" => 25, "dime" => 10, "nickel" => 05}
+  coins_back = {}
+  total_coins = 0
+
+  coins.each do |name, value|
+    coins_to_add = cents/value
+    unless coins_to_add == 0
+      total_coins += coins_to_add
+      coins_back[name] = coins_to_add
+    end
+    cents = cents % value
   end
-  test_value = test_value % value
-end
 
-if test_value > 2
-  coins_back["nickel"] += 1
-  total_coins += 1
-end
-
-# puts test_value
-# puts total_coins
-# print coins_back
-
-respond_string = ""
-
-coins_back.each_with_index do |(name, value), index|
-  if index == coins_back.length - 1 
-    respond_string += "and "
+  if cents > 2
+    coins_back["nickel"] += 1
+    total_coins += 1
   end
-  respond_string += "#{value} #{name}#{value > 1 ? "s" : ""}, "
+
+  {"dispense" => coins_back, "total" => total_coins}
+end
+
+def build_response(coins_hash)
+  respond_string = ""
+
+  coins_hash.each_with_index do |(name, value), index|
+    if index == coins_hash.length - 1 
+      respond_string += "and "
+    end
+    respond_string += "#{value} #{name}#{value > 1 ? "s" : ""}, "
+  end
+
+  "#{respond_string[0..-3]}."
 end
 
 
-puts "You need to dispense #{respond_string[0..-3]}."
-puts "Total coins: #{total_coins}"
+result = calculate_results(test_value)
+
+puts "You need to dispense #{build_response(result["dispense"])}"
+puts "Total coins: #{result["total"]}"
