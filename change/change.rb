d36af9e39f8559f8b6c7b3dcae4b78cc4ba7f7f3
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-20 13:57:34 -0400

    main code using hash for coins to be returned

--- change/change.rb
+++ change/change.rb
@@ -1,21 +1,21 @@
 coin_values_cents = [200,100,25,10,05]
-
+coin_names = ["toonie", "loonie", "quarter", "dime", "nickel"]
 test_value = 343
-coins_back = []
+coins_back = {}
 sum = 0
 
 coin_values_cents.each do |coin|
   coins_to_add = test_value/coin
   sum += coins_to_add
-  coins_back << coins_to_add
+  coins_back[coin] = coins_to_add
   test_value = test_value % coin
 end
 
 if test_value > 2
-  coins_back[-1] += 1
+  coins_back[05] += 1
   sum += 1
 end
 
 puts test_value
 puts sum
-print coins_back
+# coins_bac
