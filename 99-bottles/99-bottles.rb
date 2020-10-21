c986370f617ff01380545f9168bb435f9c977cf3
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-21 09:44:19 -0400

    solution to allow for many verses

--- 99-bottles/99-bottles.rb
+++ 99-bottles/99-bottles.rb
@@ -1,13 +1,19 @@
-last_verse = "No more bottles of beer on the wall, \nno more bottles of beer. \nGo to the store and buy some more, \n99 bottles of beer on the wall."
-
-def verse(number)
-  puts "#{number} bottle of beer on the wall,"
-  puts "#{number} bottle of beer."
-  puts "Take one down and pass it around,"
-  puts "no more bottles of beer on the wall."
-  puts ""
+def verse(number=0)
+  while number > 0
+    bottle_str = number > 1 ? "bottles" : "bottle"
+    puts "#{number} #{bottle_str} of beer on the wall,"
+    puts "#{number} #{bottle_str} of beer."
+    puts "Take one down and pass it around,"
+    if number > 1
+      puts "#{number-1} bottle#{number-1 > 1 ? "s": ""} of beer on the wall."
+    else
+      puts "no more bottles of beer on the wall." 
+    end
+    puts ""
+    number -= 1
+  end
+  puts "No more bottles of beer on the wall, \nno more bottles of beer. \nGo to the store and buy some more, \n99 bottles of beer on the wall."
 end
 
-verse(1)
+verse(0)
 
-puts last_verse
