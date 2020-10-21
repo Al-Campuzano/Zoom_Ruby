696e4f2c3413cf23f60fe696811207b89017862a
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-21 09:02:05 -0400

    last two verses

--- 99-bottles/99-bottles.rb
+++ 99-bottles/99-bottles.rb
@@ -1,3 +1,13 @@
 last_verse = "No more bottles of beer on the wall, \nno more bottles of beer. \nGo to the store and buy some more, \n99 bottles of beer on the wall."
 
+def verse(number)
+  puts "#{number} bottle of beer on the wall,"
+  puts "#{number} bottle of beer."
+  puts "Take one down and pass it around,"
+  puts "no more bottles of beer on the wall."
+  puts ""
+end
+
+verse(1)
+
 puts last_verse
