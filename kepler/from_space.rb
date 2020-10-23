76b5862cc933f338b8072927ce92bd9906335b01
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-23 08:50:26 -0400

    add organism class

--- kepler/from_space.rb
+++ kepler/from_space.rb
@@ -3,3 +3,40 @@
 # Each Organism must contain Instance Variables and Instance Methods that represent all the organism needs to know and do in order to survive
 # At least 1 method should print out information about the creature
 
+class Organism
+  # need to know: habitat(land, water, air), diet(carnivore, herbivore, omnivore, photosynthesis), type(mammal, reptile, fish, insect, plant)
+  # needs to do: hunt, flee, reproduce, eat, perish
+  # print info
+  attr_accessor :habitat
+  attr_accessor :diet
+  attr_accessor :type
+
+  def initialize
+    @habitat = "land"
+    @diet = "carnivore"
+    @type = "mammal"
+    @alive = true
+  end
+
+  def eat
+    puts @alive ? "The organism is eating." : "He's dead, Jim."
+  end
+
+  def reproduce
+    puts "The organism has reproduced."
+    offspring = self.class.new
+  end
+
+  def perish
+    puts "The organism has died."
+    @alive = false
+  end
+end
+
+org = Organism.new
+
+org.eat
+org2 = org.reproduce
+org2.eat
+org.perish
+org.eat
