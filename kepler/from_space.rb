8b501b18257cb3247228ba177cd3b22debc3e8bd
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-23 08:21:13 -0400

    initialize branch

---
+++ kepler/from_space.rb
@@ -0,0 +1,5 @@
+# Organisms
+# At least 5 distinct Organisms written as ruby Classes
+# Each Organism must contain Instance Variables and Instance Methods that represent all the organism needs to know and do in order to survive
+# At least 1 method should print out information about the creature
+
