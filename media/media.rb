e20866f649e8fe9f5981035e113a271d52ac0c61
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-26 14:49:02 -0400

    added AcceptsComments module

--- media/media.rb
+++ media/media.rb
@@ -1,15 +1,29 @@
 # Code along: pages 285-310 in Head First Ruby
 
-class Clip
-  attr_reader :comments
-
-  def initialize
-    @comments = []
-  end
+module AcceptsComments
+  def comments
+    if @comments
+      @comments
+    else
+      @comments = []
+    end
+  end  
 
   def add_comment(comment)
     comments << comment
   end
+end
+
+class Clip
+  attr_reader :comments
+
+  # def initialize
+  #   @comments = []
+  # end
+
+  # def add_comment(comment)
+  #   comments << comment
+  # end
 
   def play
     puts "Playing #{object_id}..."
@@ -17,10 +31,12 @@ class Clip
 end
 
 class Video < Clip
+  include AcceptsComments
   attr_accessor :resolution
 end
 
 class Song < Clip
+  include AcceptsComments
   attr_accessor :beats_per_minute
 end
 
@@ -32,3 +48,5 @@ song = Song.new
 song.add_comment("Awesome beat.")
 
 p video.comments, song.comments
+video.play
+song.play
