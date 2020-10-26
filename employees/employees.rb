8a45d4de8333fff2b0cdb7b3482707a348b88625
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-26 09:17:28 -0400

    add initialize method to super class

--- employees/employees.rb
+++ employees/employees.rb
@@ -3,6 +3,10 @@
 class Employee
   attr_reader :name
 
+  def initialize(name = "Anonymous")
+    self.name = name
+  end
+
   def name=(name)
     if name == ""
       raise "Name can't be blank"
@@ -19,7 +23,7 @@ class SalariedEmployee < Employee
   attr_reader :salary
 
   def initialize(name = "Anonymous", salary = 0.0)
-    self.name = name
+    super(name)
     self.salary = salary
   end
 
@@ -42,7 +46,7 @@ class HourlyEmployee < Employee
   attr_reader :hourly_wage, :hours_per_week
 
   def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
-    self.name = name
+    super(name)
     self.hourly_wage = hourly_wage
     self.hours_per_week = hours_per_week
   end
