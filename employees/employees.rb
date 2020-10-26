72ef50d1fb2b768843cc9715d018d5f446762794
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-26 09:00:49 -0400

    add SalariedEmployee class

--- employees/employees.rb
+++ employees/employees.rb
@@ -33,7 +33,34 @@ class SalariedEmployee < Employee
   end
 end
 
+class HourlyEmployee < Employee
+  attr_reader :hourly_wage, :hours_per_week
+
+  def hourly_wage=(hourly_wage)
+    raise "A wage of #{hourly_wage} isn't valid" if hourly_wage < 0
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    raise "#{hours_per_week} hours per week isn't valid" if hours_per_week < 0
+    @hours_per_week = hours_per_week
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = hourly_wage * hours_per_week * 2
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+end
+
 salaried_employee = SalariedEmployee.new
 salaried_employee.name = "Jane Doe"
 salaried_employee.salary = 50000
 salaried_employee.print_pay_stub
+
+hourly_employee = HourlyEmployee.new
+hourly_employee.name = "John Smith"
+hourly_employee.hourly_wage = 14.97
+hourly_employee.hours_per_week = 30
+hourly_employee.print_pay_stub
