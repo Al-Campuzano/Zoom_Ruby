420c4036c0c17174be3a4bb604ac0a0008c8fc07
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-27 08:52:21 -0400

    include comparable module and write tests

--- comparable-enumerable/comp-enu.rb
+++ comparable-enumerable/comp-enu.rb
@@ -1,6 +1,8 @@
 # Code along pages 311-332 in Head First Ruby
 
 class Steak
+  include Comparable
+
   attr_accessor :grade
   GRADE_SCORES = { "Prime" => 3, "Choice" => 2, "Select" => 1 }
   
@@ -15,14 +17,17 @@ class Steak
   end
 end
 
-first_steak = Steak.new
-first_steak.grade = "Prime"
-second_steak = Steak.new
-second_steak.grade = "Choice"
+prime = Steak.new
+prime.grade = "Prime"
+choice = Steak.new
+choice.grade = "Choice"
+select = Steak.new
+select.grade = "Select"
 
-# if first_steak > second_steak
-#   puts "I'll take #{first_steak.inspect}"
-# end
-
-puts first_steak <=> second_steak
-puts second_steak <=> first_steak
+puts "prime > choice: #{prime > choice}"
+puts "prime < select: #{prime < select}"
+puts "select == select: #{select == select}"
+puts "select <= select: #{select <= select}"
+puts "select >= choice: #{select >= choice}"
+print "choice.between?(select,prime): "
+puts choice.between?(select, prime)
