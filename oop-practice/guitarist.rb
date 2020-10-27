256369b60cbe38b2552454fa1c918d8609dc3719
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-27 10:43:20 -0400

    add setter and tostring methods

--- oop-practice/guitarist.rb
+++ oop-practice/guitarist.rb
@@ -11,4 +11,8 @@ puts "Jimi: #{jimi.object_id}"
 
 
 al = Musician.new("Al Campuzano", 19)
-al.name("")
+# al.name("")
+al.active(1)
+al.active(0)
+al.add_experience
+puts al
--- oop-practice/musician.rb
+++ oop-practice/musician.rb
@@ -1,10 +1,10 @@
 # Create a base class with the following specifications:
 #
-# Must have 3 attributes
+# Must have 3 attributes ✓
 # Uses proper Encapsulation
-# Uses data validation for setting attributes
+# Uses data validation for setting attributes ✓
 # Contains an array
-# Has an initialization method
+# Has an initialization method ✓
 # Contains 3 other methods
 # Uses at least 1 block
 
@@ -20,5 +20,18 @@ class Musician
   def name(name)
     raise "Name can't be blank" if name == ""
     @name = name
+  end
+
+  def add_experience
+    @years_experience += 1
+  end
+
+  def active(value)
+    @active = value ? true : false
+    puts @active
+  end
+
+  def to_s
+    "I'm a #{self.class}, my name is #{@name}. I have #{@years_experience} years of experience, and I am #{@active ? "" : "in"}active."
   end
 end
