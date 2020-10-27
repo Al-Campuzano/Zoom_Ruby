e2af30dee0396d9dfc9ef91bce96223f8e49ea69
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-27 10:32:04 -0400

    add initialize and name methods to Musician class

--- oop-practice/guitarist.rb
+++ oop-practice/guitarist.rb
@@ -1,5 +1,14 @@
+require_relative 'musician'
+
 class Guitarist < Musician
   def initialize
     puts "I'm a guitarist"
   end
 end
+
+jimi = Guitarist.new
+puts "Jimi: #{jimi.object_id}"
+
+
+al = Musician.new("Al Campuzano", 19)
+al.name("")
--- oop-practice/musician.rb
+++ oop-practice/musician.rb
@@ -1,5 +1,24 @@
+# Create a base class with the following specifications:
+#
+# Must have 3 attributes
+# Uses proper Encapsulation
+# Uses data validation for setting attributes
+# Contains an array
+# Has an initialization method
+# Contains 3 other methods
+# Uses at least 1 block
+
 class Musician
-  def initialize
+  def initialize(name = "Anonymous", years_experience = 0, active = true)
+    @name = name
+    @instruments = []
+    @years_experience = years_experience
+    @active = active
     puts "I'm a musician"
+  end
+
+  def name(name)
+    raise "Name can't be blank" if name == ""
+    @name = name
   end
 end
