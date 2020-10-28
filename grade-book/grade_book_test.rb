b4c399228c563fb7a1edbd8b76bd419ef9dadb84
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-28 15:31:22 -0400

    first test

--- grade-book/grade_book_test.rb
+++ grade-book/grade_book_test.rb
@@ -5,14 +5,21 @@
 # Identify any exceptional/error cases.
 
 # A test that uses refute_equal
+# A test that uses assert_includes
 # A test that uses assert_output
 # A test that uses refute_nil
-# A test that uses assert_instance_of
+# A test that uses refute_empty
+# A test that uses assert_kind_of
+# A test that uses assert_raises
+# A test that uses assert_instance_of ✓
 # at least 6 total, one for each method
 
 require 'minitest/autorun'
 require_relative 'grade_book'
 
 class GradeBookTest < Minitest::Test
-  
+  def test_init_of_gradebook_sets_up_correct_object
+    grade_book = GradeBook.new
+    assert_instance_of(GradeBook, grade_book, msg = "grade_book is not a GradeBook object")
+  end
 end
