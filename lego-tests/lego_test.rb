ce9e4442222db5286544acaf6eb9858059fd6332
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-30 01:00:52 -0400

    add tests for LegoItem

--- lego-tests/lego_test.rb
+++ lego-tests/lego_test.rb
@@ -3,18 +3,28 @@ require_relative 'lego'
 
 class LegoTest < Minitest::Test
   # LegoHat tests
+  @@hat = LegoHat.new("Large", "Red", "Sombrero")
   def test_initializing_LegoHat_returns_correct_class
-    assert_instance_of(LegoHat, LegoHat.new("Large", "Red", "Sombrero"))
+    assert_instance_of(LegoHat, @@hat)
   end
 
   def test_LegoHat_to_s_returns_correct_string
     expected = "a Large Red Sombrero"
-    hat = LegoHat.new("Large", "Red", "Sombrero")
-    assert(hat.to_s == expected)
+    assert(@@hat.to_s == expected)
   end
 
   # LegoItem tests
+  @@item = LegoItem.new("Sword", 8)
   def test_init_LegoItem_returns_correct_class
-    assert_instance_of(LegoItem, LegoItem.new("Sword", 8))
+    assert_instance_of(LegoItem, @@item)
+  end
+
+  def test_LegoItem_to_s_returns_correct_string
+    expected = "a 8 gram Sword"
+    assert(@@item.to_s == expected)
+  end
+
+  def test_LegoItem_is_heavy_returns_boolean
+    assert_kind_of(FalseClass, @@item.is_heavy(10))
   end
 end
