c0b2eae8ae558bc47ab90626f91c82f2be5d6d20
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-30 02:50:14 -0400

    test that draw returns a string

--- ca-tdd/ascii_art.rb
+++ ca-tdd/ascii_art.rb
@@ -1,2 +1,9 @@
 # draw a diamond of asterisks that is centered, 
 # with a maximum width of the passed number.
+# You will need to design your code using TDD. To help with testing your code, be sure to create an AsciiArt class with a draw(number) method that returns a string that is correctly formatted so that it could be used with puts to draw the desired shape.
+
+class AsciiArt
+  def self.draw(number)
+    "string"
+  end
+end
--- ca-tdd/ascii_art_tests.rb
+++ ca-tdd/ascii_art_tests.rb
@@ -2,4 +2,7 @@ require 'minitest/autorun'
 require_relative 'ascii_art.rb'
 
 class AsciiArtTest < Minitest::Test
+  def test_draw_returns_string
+    assert_kind_of(String, AsciiArt.draw(2))
+  end
 end
