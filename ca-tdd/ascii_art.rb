4b668dbe74f63942d3e358526caa91e5d9a88f66
Alberto Campuzano <al.campuzano@shopify.com>
2020-10-31 19:01:17 -0400

    5 tests and solution

--- ca-tdd/ascii_art.rb
+++ ca-tdd/ascii_art.rb
@@ -2,8 +2,28 @@
 # with a maximum width of the passed number.
 # You will need to design your code using TDD. To help with testing your code, be sure to create an AsciiArt class with a draw(number) method that returns a string that is correctly formatted so that it could be used with puts to draw the desired shape.
 
+#  *
+# * *
+#  *
+
 class AsciiArt
   def self.draw(number)
-    "string"
+    # " * \n* * \n * \n"
+    result = ""
+    (1..number).each do |i|
+      result += " " * (number - i)
+      result += "* " * i
+      result += "\n"
+    end
+
+    (number-1).downto(1) do |i|
+      result += " " * (number - i)
+      result += "* " * i
+      result += "\n"
+    end
+
+    result
   end
 end
+
+# puts AsciiArt.draw(1)
