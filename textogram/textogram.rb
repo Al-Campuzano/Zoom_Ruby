1125c5e8a914bb9f556abfff3b601422c16db9d4
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-02 12:47:19 -0500

    add to_s method

--- textogram/textogram.rb
+++ textogram/textogram.rb
@@ -7,17 +7,29 @@ class Textogram
     @histogram = Hash.new(0)
   end
 
-  def make_histogram(case_sensitive=true, only_alphanum=false)
+  def make_histogram(case_sensitive=true, special_chars=true)
     text_arr = @text.split.join("").split("")
-    text_arr = text_arr.select { |c| c.downcase != c.upcase } if only_alphanum
+    text_arr = text_arr.select { |c| c.downcase != c.upcase } unless special_chars
     text_arr = text_arr.map { |c| c.downcase } unless case_sensitive
     
     @histogram = text_arr.tally
   end
+
+  def histogram
+    @histogram
+  end
+
+  def to_s
+    result = ""
+    @histogram.sort.each do |c|
+      result += "#{c[0]} #{'*' * c[1]}\n"
+    end
+    result
+  end
 end
 
 text = Textogram.new("HELLO, world!")
-histogram = x.make_histogram
+text.make_histogram(false,true)
+puts text
 
-
-p histogram
+# p histogram.sort
