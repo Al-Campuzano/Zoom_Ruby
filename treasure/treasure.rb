c7b4245526ca88f0e1d3a0f764b7b63ad95199f1
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-02 15:13:18 -0500

    initialize branch

---
+++ treasure/treasure.rb
@@ -0,0 +1,3 @@
+# Write a TreasureMap class that stores a hash as per above. Add methods to add or remove food sources, water sources, or treasure to or from the map.
+
+# Add a to_s method to TreasureMap that displays all the characters of the map. You will need to iterate the hash to find the bounds of the map first such that you know the upper-, bottom-, left-, and right-most coordinates of the food/water/treasure. Then you can print out the values to the screen, checking the hash as you go to see what to print at each coordinate.
