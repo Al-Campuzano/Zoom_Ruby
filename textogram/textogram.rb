6bdb70fbe0883cbf82436289e695ff376649be6e
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-02 12:38:28 -0500

    add Textogram class and make_histogram method

--- textogram/textogram.rb
+++ textogram/textogram.rb
@@ -1,1 +1,23 @@
 # histogram of text
+# puts [1, 1, 2].tally
+
+class Textogram
+  def initialize(text)
+    @text = text
+    @histogram = Hash.new(0)
+  end
+
+  def make_histogram(case_sensitive=true, only_alphanum=false)
+    text_arr = @text.split.join("").split("")
+    text_arr = text_arr.select { |c| c.downcase != c.upcase } if only_alphanum
+    text_arr = text_arr.map { |c| c.downcase } unless case_sensitive
+    
+    @histogram = text_arr.tally
+  end
+end
+
+text = Textogram.new("HELLO, world!")
+histogram = x.make_histogram
+
+
+p histogram
