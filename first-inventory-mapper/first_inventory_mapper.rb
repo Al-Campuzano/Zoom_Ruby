08ec9454ddd87e7e627e646e3694597f9c9eafff
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-03 08:48:02 -0500

    add tests. It works

--- first-inventory-mapper/first_inventory_mapper.rb
+++ first-inventory-mapper/first_inventory_mapper.rb
@@ -12,35 +12,32 @@
 # Top: part ID = "d"
 # Legs: part ID = "e"
 
-# "abccc" => {"Shelf" : 1, "Stool": 1, "Table": 0}
-# "beceadee" => {"Shelf" : 1, "Stool": 0, "Table": 1}
-# "eebeedebaceeceedeceacee" => {"Shelf" : 2, "Stool": 1, "Table": 2}
-# "zabc" => {"Shelf" : 1, "Stool" : 0, "Table" : 0}
-# "deeedeee" => {"Shelf" : 0, "Stool" : 0, "Table" : 1}
 
 class InventoryMapper
   def initialize
     @shelves = @stools = @tables = 0
     @parts_map = { "a" => 1, "b" => 1, "c" => 3, "d" => 1, "e" => 4 }
   end
-
-  def map(input)
+  
+  def map(input="")
+    @shelves = @stools = @tables = 0
     @input_map = input.split("").tally
     self.find_shelves if @input_map.has_key?("a")
     self.find_stools if @input_map["b"] && @input_map["c"]
     self.find_tables if @input_map["d"] && @input_map["e"]
-
+    
     result = { "Shelf" => @shelves, "Stool" => @stools, "Table" => @tables }
+    print "\"#{input}\" => "
     p result
   end
-
+  
   def find_shelves
     while @input_map["a"] >= 1
       @input_map["a"] -= 1
       @shelves += 1
     end
   end
-
+  
   def find_stools
     while @input_map["b"] >= 1 && @input_map["c"] >= 3
       @input_map["b"] -= 1
@@ -48,7 +45,7 @@ class InventoryMapper
       @stools += 1
     end
   end
-
+  
   def find_tables
     while @input_map["d"] >= 1 && @input_map["e"] >= 4
       @input_map["d"] -= 1
@@ -58,5 +55,18 @@ class InventoryMapper
   end
 end
 
+# "abccc" => {"Shelf" : 1, "Stool": 1, "Table": 0}
+# "beceadee" => {"Shelf" : 1, "Stool": 0, "Table": 1}
+# "eebeedebaceeceedeceacee" => {"Shelf" : 2, "Stool": 1, "Table": 2}
+# "zabc" => {"Shelf" : 1, "Stool" : 0, "Table" : 0}
+# "deeedeee" => {"Shelf" : 0, "Stool" : 0, "Table" : 1}
+
 mapper = InventoryMapper.new
 mapper.map("abccc")
+mapper.map("beceadee")
+mapper.map("eebeedebaceeceedeceacee")
+mapper.map("zabc")
+mapper.map("deeedeee")
+mapper.map("")
+mapper.map
+
