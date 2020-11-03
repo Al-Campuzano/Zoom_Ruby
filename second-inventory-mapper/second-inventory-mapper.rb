43c3efc508e427005a1862ce717bf2dc7a16c5ea
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-03 10:05:15 -0500

    add product superclass and subclasses

--- second-inventory-mapper/second-inventory-mapper.rb
+++ second-inventory-mapper/second-inventory-mapper.rb
@@ -17,3 +17,51 @@
 # "eebeedebaceeceedeceacee" => {"Shelf" : 2, "Stool": 1, "Table": 2}
 # "zabc" => {"Shelf" : 1, "Stool" : 0, "Table" : 0}
 # "deeedeee" => {"Shelf" : 0, "Stool" : 0, "Table" : 1}
+
+class Product
+  attr_reader :total
+  
+  def initialize
+    @total = 0
+    @parts = {"b" => 1, "c" => 3}
+  end
+
+  def calculate_total(input)
+    @total = 0
+    @temp_hash = Hash.new(0)
+    @parts.each do |k,v|
+      if input[k] && (input[k] / v) >= 1
+        @temp_hash[k] = input[k] / v
+      end
+    end
+    if @temp_hash.size == @parts.size
+      @total = @temp_hash.sort_by {|k,v| v}[0][1]
+    end
+    @total
+  end
+end
+
+class Shelf < Product
+  def initialize
+    @total = 0
+    @parts = { "a"=>1 }
+  end
+end
+
+class Stool < Product
+  def initialize
+    @total = 0
+    @parts = { "b"=>1, "c"=>3 }
+  end
+end
+
+class Table < Product
+  def initialize
+    @total = 0
+    @parts = { "d"=>1, "e"=>4 }
+  end
+end
+
+input = "abbbbccc".split("").tally
+prod = Product.new
+print prod.calculate_total(input)
