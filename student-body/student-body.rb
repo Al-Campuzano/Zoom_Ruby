a7b9b7b500c5917ca3db378f27bc1644a5e28f7f
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-04 08:57:55 -0500

    removed testing code and comments from student-body.rb file

--- student-body/student-body.rb
+++ student-body/student-body.rb
@@ -1,9 +1,3 @@
-# In this exercise, you will create a class that mixes in Enumerable.
-#
-# Implement an each method in StudentBody. It should yield to a block for every student in the school. The idea is that the methods that come with Enumerable, such as map, will use each to accomplish their tasks, so the block we are talking about here is an arbitrary one that is passed in through those methods, or by someone directly using each on your class.
-#
-# Write Minitest tests to check that your class behaves correctly. Be sure to cover one case in each test, and to cover as many cases as possible.
-
 class StudentBody
   include Enumerable
 
@@ -30,16 +24,3 @@ class StudentBody
     end
   end
 end
-
-sb = StudentBody.new
-
-sb.each { |x| puts x }
-p sb.find_all { |x| x.include?("on")}
-p sb.count
-p sb.first
-p sb.sort
-p sb.sort.last
-p sb.reject { |x| x.downcase.include?("a") }
-short = StudentBody.new({ grade8: ["Claudio", "Mic"] })
-p short.map { |x| x.reverse }
-short.each { |x| puts x }
