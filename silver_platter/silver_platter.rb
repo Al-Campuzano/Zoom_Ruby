dbd13a30385b1b5c9e4eb60dabe665081754efe8
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-05 09:37:26 -0500

    initialize branch

---
+++ silver_platter/silver_platter.rb
@@ -0,0 +1,7 @@
+# Define the following classes:
+# Fruit, which implements a custom to_s method
+# Apple, which extends Fruit, overrides the to_s method and implements an eat method
+# Banana, which extends Fruit, overrides the to_s method and implements an eat method
+# Cheese, which implements a custom to_s method as well as an eat method
+# Sausage, which implements a custom to_s method as well as an eat method
+
