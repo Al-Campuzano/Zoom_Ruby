8b2d02a0f3f061a1c92224e8c33c686c8281f709
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-05 09:47:14 -0500

    add testing code for second assignment question

--- silver_platter/silver_platter.rb
+++ silver_platter/silver_platter.rb
@@ -1,4 +1,5 @@
-
+# Under the class definitions, create a variable called silver_platter that stores a list of at least one instance of each class you defined above. Then, iterate through the list using each, first printing out the object using its to_s, then calling the eat method. Did you run into any problems? Why or why not? (Include your written answer in your PR description.)
+# silver_platter.rb:53:in `block in <main>': undefined method `eat' for #<Fruit:0x00007ff1dc114a38> (NoMethodError)
 
 class Fruit
   def to_s
@@ -44,4 +45,11 @@ class Sausage
   def eat
     puts "These jokes are the wurst!"
   end
+end
+
+silver_platter = [Fruit.new, Apple.new, Banana.new, Cheese.new, Sausage.new]
+
+silver_platter.each do |food|
+  puts food
+  food.eat
 end
