17987bb13d73a59b2733373470ed8a753350f221
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-05 09:54:08 -0500

    removed Fruit object from test list to avoid NoMethodError

--- silver_platter/silver_platter.rb
+++ silver_platter/silver_platter.rb
@@ -1,6 +1,11 @@
 # Under the class definitions, create a variable called silver_platter that stores a list of at least one instance of each class you defined above. Then, iterate through the list using each, first printing out the object using its to_s, then calling the eat method. Did you run into any problems? Why or why not? (Include your written answer in your PR description.)
 # silver_platter.rb:53:in `block in <main>': undefined method `eat' for #<Fruit:0x00007ff1dc114a38> (NoMethodError)
 
+# Remove the error by updating what is in your silver_platter list. What did you have to remove, and why? (Include your written answer in your PR description.)
+# I had to remove the Fruit object from the silver_platter list because that class/object does not have an _eat_ method.
+
+# Finally, add whatever you removed back to the list, and use respond_to? to fix the error.
+
 class Fruit
   def to_s
     "I'm a juicy #{self.class}."
@@ -13,7 +18,7 @@ class Apple < Fruit
   end
 
   def eat
-    puts "Green apples are my favorite!"
+    puts "How do you like them apples?"
   end
 end
 
@@ -23,7 +28,7 @@ class Banana < Fruit
   end
 
   def eat
-    puts "You gotta peel me first!"
+    puts "Eating me is so appealing!"
   end
 end
 
@@ -47,7 +52,7 @@ class Sausage
   end
 end
 
-silver_platter = [Fruit.new, Apple.new, Banana.new, Cheese.new, Sausage.new]
+silver_platter = [Apple.new, Banana.new, Cheese.new, Sausage.new]
 
 silver_platter.each do |food|
   puts food
