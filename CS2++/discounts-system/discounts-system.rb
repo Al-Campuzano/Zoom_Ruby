b171b2ede3d0d214fb97c596fbb725c7fc09cd01
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-20 09:32:48 -0500

    changed classes to modules. Now using direct methods instead of instance methods

--- CS2++/discounts-system/discounts-system.rb
+++ CS2++/discounts-system/discounts-system.rb
@@ -1,21 +1,17 @@
 # discounts-system.rb
 
-class Grape
-  def initialize(price_cents: 500)
-    @price_cents = price_cents
-  end
-
-  def apply_discounts(quantity)
+module Grapes
+  @price_cents = 500
+  
+  def self.apply_discounts(quantity)
     (quantity/2.0).round * @price_cents
   end
 end
 
-class Apple
-  def initialize(price_cents: 300)
-    @price_cents = price_cents
-  end
-
-  def apply_discounts(quantity)
+module Apples
+  @price_cents = 300
+  
+  def self.apply_discounts(quantity)
     if quantity >= 2
       @price_cents * quantity * 0.8
     else
@@ -24,22 +20,26 @@ class Apple
   end
 end
 
-class Peach
-  def initialize(price_cents: 700)
-    @price_cents = price_cents
-  end
+module Peaches
+  @price_cents = 700
 
-  def apply_discounts(quantity)
+  def self.apply_discounts(quantity)
     @price_cents * quantity
   end
 end
 
 
-peach = Peach.new
-puts peach.apply_discounts(2)
+# peach = Peach.new
+puts Peaches.apply_discounts(2)
 
-apple = Apple.new
-puts apple.apply_discounts(10)
+# apple = Apple.new
+puts Apples.apply_discounts(10)
 
-grape = Grape.new
-puts grape.apply_discounts(4)
+# grape = Grape.new
+puts Grapes.apply_discounts(4)
+
+# puts Kernel.const_get("peach".capitalize)
+# puts Object.const_set("Grape", Class.new).apply_discounts(2)
+
+# puts Testing.apply_discounts(2)
+# puts Kernel.const_get("testing".capitalize).apply_discounts(2)
