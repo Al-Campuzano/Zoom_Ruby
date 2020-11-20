2b99fbb4b8a8f7a255f374a2ddbbbee80a7fa4ab
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-20 08:55:13 -0500

    add basic structure of Grape, Apple, and Peach classes

--- CS2++/discounts-system/discounts-system.rb
+++ CS2++/discounts-system/discounts-system.rb
@@ -1,1 +1,31 @@
 # discounts-system.rb
+
+class Grape
+  def initialize(price_cents: 500)
+    @price_cents = price_cents
+  end
+
+  def apply_discounts(quantity)
+    
+  end
+end
+
+class Apple
+  def initialize(price_cents: 300)
+    @price_cents = price_cents
+  end
+
+  def apply_discounts(quantity)
+    
+  end
+end
+
+class Peach
+  def initialize(price_cents: 700)
+    @price_cents = price_cents
+  end
+
+  def apply_discounts(quantity)
+    
+  end
+end
