6e47297119bb4089551d71872639f26e598c2990
Alberto Campuzano <al.campuzano@shopify.com>
2020-11-27 10:01:00 -0500

    add test for conjured items. add functionality to deal with conjured items. refactor

--- CS2++/GildedRose-Refactoring-Kata-master/ruby/gilded_rose.rb
+++ CS2++/GildedRose-Refactoring-Kata-master/ruby/gilded_rose.rb
@@ -6,46 +6,35 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
+      unless item.name.include? "Sulfuras"
+        if item.name.include?("Aged Brie") || item.name.include?("Backstage passes")
+          if item.quality < 50
+            item.quality += 1
+            if item.name.include?("Backstage passes") && item.quality < 50
+              if item.sell_in < 11
+                item.quality += 1
+              elsif item.sell_in < 6
+                item.quality += 2
               end
             end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
+          if item.quality > 0
+            item.name.include?("Conjured") ? item.quality = [item.quality - 2, 0].max : item.quality -= 1
+          end
+        end
+        
+        item.sell_in -= 1
+        
+        if item.sell_in < 0
+          if item.name.include? "Aged Brie"
+            item.quality = [item.quality + 1, 50].min
+          else
+            if item.name.include? "Backstage passes"
+              item.quality = 0
+            else
+              item.quality = [item.quality - 1, 0].max
+            end
           end
         end
       end
--- CS2++/GildedRose-Refactoring-Kata-master/ruby/gilded_rose_tests.rb
+++ CS2++/GildedRose-Refactoring-Kata-master/ruby/gilded_rose_tests.rb
@@ -6,7 +6,14 @@ class TestUntitled < Test::Unit::TestCase
   def test_foo
     items = [Item.new("foo", 0, 0)]
     GildedRose.new(items).update_quality()
-    assert_equal items[0].name, "fixme"
+    assert_equal items[0].name, "foo"
+  end
+
+  def test_conjured_item
+    quality = 6
+    items = [Item.new("This is a Conjured test", 3, quality)]
+    GildedRose.new(items).update_quality
+    assert_equal items[0].quality, quality - 2
   end
 
 end
