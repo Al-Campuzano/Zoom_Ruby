e8f7625b566715fa3e439f6168043b7d9298eb6a
Alberto Campuzano <al.campuzano@shopify.com>
2020-12-03 11:41:43 -0500

    implement OR conditions

--- CS2++/search-engine/search-engine.rb
+++ CS2++/search-engine/search-engine.rb
@@ -33,10 +33,18 @@ loop do
   end
 
   results = nil
+  # AND implementation
+  # user_input.split.each do |user_word|
+  #   temp_results = word_map[user_word] || []
+  #   results ||= temp_results
+  #   results = results & temp_results
+  # end
+
+  # OR implementation
   user_input.split.each do |user_word|
     temp_results = word_map[user_word] || []
     results ||= temp_results
-    results = results & temp_results
+    results = results|temp_results
   end
   
   if results.size > 0
